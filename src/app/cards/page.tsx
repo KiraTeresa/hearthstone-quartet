eab51a29cfe92932a43727a7b4efd35051137245
KiraTeresa <kira.soba@gmx.de>
2024-03-17 10:12:24 +0100

    prepare getAllCardsService

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,7 +1,11 @@
 import CardV1Component from "@/app/components/card/card-v1/card-v1.component";
 import styles from './cards-page.module.scss';
+import getAllCardsService from "@/app/services/get-all-cards.service";
 
-const CardsPage = () => {
+const CardsPage = async () => {
+	// const allCards = await getAllCardsService();
+	// console.log(allCards);
+
 	return (
 		<>
 			<h2 className={styles.headline}>Cards</h2>
