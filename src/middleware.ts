3d7f2c06f265e2134b4e6e8795a4bb1f74833c88
Kira <kira.soba@gmx.de>
2024-03-24 17:33:56 +0100

    set Reporting-Endpoints as response header

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEN_URL || 'http://localhost:8080';
 
-export function middleware( request: NextRequest ) {
-    const nonce = Buffer.from( crypto.randomUUID() ).toString( 'base64' );
+export function middleware(request: NextRequest) {
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
     // ! unsafe-eval is not recommended, but without it the button does not function at all
     const cspHeader = `
     default-src 'self';
@@ -23,22 +23,23 @@ export function middleware( request: NextRequest ) {
     `;
     // Replace newline characters and spaces
     const cspHeaderValue = cspHeader
-        .replace( /\s{2,}/g, ' ' )
+        .replace(/\s{2,}/g, ' ')
         .trim();
 
-    const requestHeaders = new Headers( request.headers );
-    requestHeaders.set( 'x-nonce', nonce );
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set('x-nonce', nonce);
 
-    requestHeaders.set( 'Reporting-Endpoints', `csp-endpoint="${BACKEND_URL}/cps-reports"` );
-    requestHeaders.set( 'Content-Security-Policy', cspHeaderValue );
+    requestHeaders.set('Reporting-Endpoints', `csp-endpoint="${BACKEND_URL}/cps-reports"`);
+    requestHeaders.set('Content-Security-Policy', cspHeaderValue);
 
-    const response = NextResponse.next( {
+    const response = NextResponse.next({
         request: {
             headers: requestHeaders,
         }
-    } );
+    });
 
-    response.headers.set( 'Content-Security-Policy', cspHeaderValue );
+    response.headers.set('Reporting-Endpoints', `csp-endpoint="${BACKEND_URL}/cps-reports"`);
+    response.headers.set('Content-Security-Policy', cspHeaderValue);
 
     return response;
 }
